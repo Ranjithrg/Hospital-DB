9d9d712c0dc833939d90070d7cdaa4fdc141145a
RANJITH R G <95525560+Ranjithrg@users.noreply.github.com>
2024-10-07 18:03:30 +0530

    Modification.sql

--- Modification.sql
+++ Modification.sql
@@ -7,4 +7,23 @@ SELECT address, COUNT(*) AS number
 FROM Patient
 GROUP BY address;
 
+-- Find Patient with exactly 5-character names
+
+SELECT *
+FROM patient
+WHERE email LIKE '__n%';
+
+-- Find Customers whose names start with 
+SELECT *
+FROM patient
+WHERE email LIKE 'g%';
+
+/*======================== Retrieve Orders with  Details ======================*/
+select patient,appt,concerns,symptoms
+  from PatientsAttendAppointments 
+  left join Patient 
+  on PatientsAttendAppointments.patient = Patient.email;
+
+
+
 
